f8cdc285fbb5c1792c8071806ef49b7adfe15881
acordray79 <acordray79@gmail.com>
2019-04-29 16:20:55 -0400

    Added Restaurant Update Controller and View

--- RestarauntRaterMVC/Controllers/RestaurantController.cs
+++ RestarauntRaterMVC/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestarauntRaterMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -38,6 +39,34 @@ namespace RestarauntRaterMVC.Controllers
             return View(restaurant);
         }
 
+        // GET: Restaurant/Edit
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        //POST: Restaurant/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
         // GET: Restaurant/Delete
         public ActionResult Delete(int? id)
         {
